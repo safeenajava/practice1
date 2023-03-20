705914a844709acbf1be97629591ca502d0cd421
TEST <TEST@DESKTOP-RBM33K7.mynetworksettings.com>
2023-03-20 12:22:10 -0400

    Assignment for operator

---
+++ src/practiceOperators/Arithmatic.java
@@ -0,0 +1,21 @@
+package practiceOperators;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+		int i = 10; 
+		int j = 7; 
+
+		System.out.println(i+j); 
+
+		System.out.println(i-j); 
+
+		System.out.println(i*j); 
+
+		System.out.println(i/j); 
+
+		System.out.println(i%j);
+
+	}
+
+}
